c0e118db787e14b2625444cb16f16ce68690ecf4
Dag Haug <daghaug@ifikk.uio.no>
2025-04-08 14:56:03 +0200

    Ny versjon av first_exmample

--- first_example.py
+++ first_example.py
@@ -1,12 +1,25 @@
 from llama_cpp import Llama
 
-llm = Llama(
-      model_path="/itf-fi-ml/shared/ml-models/NoraLLm/normistral-7b-warm.Q8_0.gguf", 
-      n_threads=8,
-      n_gpu_layers=-1,
-      # seed=1337, # Uncomment to set a specific seed
-      n_ctx=2048, # Set context window size
+# hvis du bruker ferdignedlastede modeller på ml-nodene bruker du denne linja
+# llm = Llama(
+#    model_path="/itf-fi-ml/shared/ml-models/NoraLLm/normistral-7b-warm.Q8_0.gguf",
+#    n_threads=8,
+#    n_gpu_layers=32,
+#    # seed=1337, # Uncomment to set a specific seed
+#    n_ctx=2048, # Set context window size
+#)
+
+
+# Og hvis du skal laste ned modellen fra HugginFace bruker du denne
+llm = Llama.from_pretrained(
+    repo_id="norallm/normistral-7b-warm",  # HuggingFace repository containing the GGUF files.
+    filename="*Q8_0.gguf", # suffix of the filename containing the level of quantization. 
+    n_threads=8,
+    n_gpu_layers=32,
+    # seed=1337, # Uncomment to set a specific seed
+    n_ctx=2048, # Set context window size
 )
+
 
 # Simple inference example
 output = llm(
